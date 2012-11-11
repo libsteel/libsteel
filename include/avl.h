193ef3f2338b43582cbc2374fe47080ab4583422
David Majnemer <david.majnemer@gmail.com>
2012-11-11 14:54:46 -0800

    [avl] add avl_search, rename some avl functions

--- include/avl.h
+++ include/avl.h
@@ -3,23 +3,29 @@
 
 #include "steel.h"
 
+struct steel_avl_node {
+  struct steel_avl_node san_children[2];
+};
+typedef struct steel_avl_node steel_avl_node_t;
+
 struct steel_avl_tree {
   size_t sat_count;
   size_t sat_elem_size;
   size_t sat_link_offset;
 
+  steel_avl_node_t sat_root;
+
   steel_ordered_comparator_t sat_comparator;
   steel_allocator_t          sat_allocator;
   steel_deallocator_t        sat_deallocator;
 };
-
 typedef steel_avl_tree steel_avl_tree_t;
 
 
-void steel_avl_create(steel_avl_tree_t *sat, size_t elem_size, size_t link_offset,
-                      steel_ordered_comparator_t comparator, steel_allocator_t allocator,
-                      steel_deallocator_t deallocator);
+void steel_avl_init(steel_avl_tree_t *sat, size_t elem_size, size_t link_offset,
+                    steel_ordered_comparator_t comparator, steel_allocator_t allocator,
+                    steel_deallocator_t deallocator);
 
-void steel_avl_destroy(steel_avl_tree_t *sat);
+void steel_avl_fini(steel_avl_tree_t *sat);
 
 #endif  // STEEL_AVL_H_
--- src/avl.c
+++ src/avl.c
@@ -1,17 +1,44 @@
 #include "avl.h"
 
-void steel_avl_create(steel_avl_tree_t *sat, size_t elem_size, size_t link_offset,
-                      steel_ordered_comparator_t comparator, steel_allocator_t allocator,
-                      steel_deallocator_t deallocator) {
+#include <stdint.h>
+
+void steel_avl_init(steel_avl_tree_t *sat, size_t elem_size, size_t link_offset,
+                    steel_ordered_comparator_t comparator, steel_allocator_t allocator,
+                    steel_deallocator_t deallocator) {
   sat->sat_count = 0;
   sat->sat_elem_size = elem_size;
   sat->sat_link_offset = link_offset;
+
+  sat->sat_root = NULL;
 
   sat->sat_comparator = comparator;
   sat->sat_allocator = allocator;
   sat->sat_deallocator = deallocator;
 }
 
-void steel_avl_destroy(steel_avl_tree_t *sat) {
-	(void) sat;
+void *steel_avl_node_to_elem(const steel_avl_tree *sat, steel_avl_node_t *san) {
+  return (void *)((uintptr_t)san - sat->sat_link_offset);
+}
+
+void *steel_avl_search(steel_avl_tree *sat, const void *key) {
+  steel_avl_node_t *node;
+
+  node = sat->sat_root;
+  while (node != NULL) {
+    int cmp;
+
+    cmp = sat->sat_comparator(key, steel_avl_node_to_elem(sat, node));
+    if (cmp < 0) {
+      node = sat->sat_comparator[0];
+    } else if (cmp > 0) {
+      node = sat->sat_comparator[1];
+    } else {
+      break;
+    }
+  }
+  return steel_avl_node_to_elem(sat, node);
+}
+
+void steel_avl_fini(steel_avl_tree_t *sat) {
+  (void) sat;
 }
