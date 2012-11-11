f52139d8deb283fc29829932d20ee7005c7ec0f9
David Majnemer <david.majnemer@gmail.com>
2012-11-11 14:26:46 -0800

    [avl] Starting work on AVL trees

---
+++ include/avl.h
@@ -0,0 +1,23 @@
+#ifndef STEEL_AVL_H_
+#define STEEL_AVL_H_
+
+#include "steel.h"
+
+struct steel_avl_tree {
+  size_t sat_count;
+  size_t sat_elem_size;
+  size_t sat_link_offset;
+
+  steel_ordered_comparator_t sat_comparator;
+  steel_allocator_t          sat_allocator;
+  steel_deallocator_t        sat_deallocator;
+};
+
+typedef steel_avl_tree steel_avl_tree_t;
+
+
+void steel_avl_create(steel_avl_tree_t *sat, size_t elem_size, size_t link_offset,
+                      steel_ordered_comparator_t comparator, steel_allocator_t allocator,
+                      steel_deallocator_t deallocator);
+
+#endif  // STEEL_AVL_H_
---
+++ src/avl.c
@@ -0,0 +1,13 @@
+#include "avl.h"
+
+void steel_avl_create(steel_avl_tree_t *sat, size_t elem_size, size_t link_offset,
+                      steel_ordered_comparator_t comparator, steel_allocator_t allocator,
+                      steel_deallocator_t deallocator) {
+  sat->sat_count = 0;
+  sat->sat_elem_size = elem_size;
+  sat->sat_link_offset = link_offset;
+
+  sat->sat_comparator = comparator;
+  sat->sat_allocator = allocator;
+  sat->sat_deallocator = deallocator;
+}
