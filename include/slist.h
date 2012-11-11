c7458c20e6ef1597bf299b2e80de8e28137b2374
Kurtis Nusbaum <klnusbaum@gmail.com>
2012-11-11 15:24:04 -0800

    [slist] added slist insert

--- include/slist.h
+++ include/slist.h
@@ -12,19 +12,16 @@ typedef struct steel_slist{
   size_t ssl_elem_size;
   size_t ssl_link_offset
 
-  steel_allocator_t ssl_allocator,
-  steel_deallocator_t ssl_deallocator;
-
   steel_slist_node_t *head;
   steel_slist_node_t *tail;
 };
 typedef struct steel_slist steel_slist_t;
 
-void steel_slist_init(steel_slist_t *slist, size_t elem_size, size_t link_offset,
-                      steel_allocator_t allocator, steel_deallocator_t deallocator);
+void steel_slist_init(steel_slist_t *slist, size_t elem_size, size_t link_offset);
 
 void steel_slist_fini(steel_slist_t *slist);
 
+void steel_slist_insert(steel_slist_t *slist, void *elem) {
 
 
 #endif //STEEL_SINGLY_LINKED_LIST_H
--- src/slist.c
+++ src/slist.c
@@ -2,13 +2,9 @@
 
 #include "assert.h"
 
-void steel_slist_init(steel_slist_t *slist, size_t elem_size, size_t link_offset,
-                      steel_allocator_t allocator, steel_deallocator_t deallocator) {
+void steel_slist_init(steel_slist_t *slist, size_t elem_size, size_t link_offset) {
   slist->ssl_elem_size = elem_size;
   slist->ssl_link_offset = link_offset;
-
-  slist->ssl_allocator = allocator;
-  slist->ssl_deallocator = deallocator;
 
   slist->head = NULL;
   slist->tail = NULL;
@@ -17,4 +13,24 @@ void steel_slist_init(steel_slist_t *slist, size_t elem_size, size_t link_offset
 void steel_slist_fini(steel_slist_t *slist) {
   assert(slist->head == NULL);
   assert(slist->tail == NULL);
+}
+
+steel_slist_node_t *steel_slist_elem_to_node(const steel_slist_t *slist, void *elem) {
+  return (steel_slist_node_t *)((uintprt_t)elem + slist->ssl_elem_offset);
+}
+
+void steel_slist_insert(steel_slist_t *slist, void *elem) {
+  steel_slist_node_t *node;
+  node = steel_slist_elem_to_node(slist, elem);
+
+  if(slist->head == NULL){
+    slist->head = node;
+    slist->tail = node;
+  }
+  else{
+    slist->tail->next = node;
+    slist->tail = node;
+  }
+
+  node->next = NULL;
 }
