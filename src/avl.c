9e9b1c96763b64d07a2128e9c563ed87ad44b79e
David Majnemer <david.majnemer@gmail.com>
2012-11-11 23:25:04 -0800

    [avl] insert is almost done, left is rotations

--- src/avl.c
+++ src/avl.c
@@ -4,6 +4,22 @@
 #include <stddef.h>
 #include <stdint.h>
 
+static void *steel_avl_node_to_elem(const steel_avl_tree_t *sat, steel_avl_node_t *san) {
+  return (void *)((uintptr_t)san - sat->sat_link_offset);
+}
+
+static steel_avl_node_t *steel_avl_elem_to_node(const steel_avl_tree_t *sat, void *elem) {
+  return (steel_avl_node_t *)((uintptr_t)elem + sat->sat_link_offset);
+}
+
+void steel_avl_rotate_left(const steel_avl_tree_t *sat, steel_avl_node_t *san) {
+  // TODO: fill in!
+}
+
+void steel_avl_rotate_right(const steel_avl_tree_t *sat, steel_avl_node_t *san) {
+  // TODO: fill in!
+}
+
 void steel_avl_init(steel_avl_tree_t *sat, size_t elem_size, size_t link_offset,
                     steel_ordered_comparator_t comparator) {
   sat->sat_count = 0;
@@ -13,14 +29,6 @@ void steel_avl_init(steel_avl_tree_t *sat, size_t elem_size, size_t link_offset,
   sat->sat_root = NULL;
 
   sat->sat_comparator = comparator;
-}
-
-static void *steel_avl_node_to_elem(const steel_avl_tree *sat, steel_avl_node_t *san) {
-  return (void *)((uintptr_t)san - sat->sat_link_offset);
-}
-
-static steel_avl_node_t *steel_avl_elem_to_node(const steel_avl_tree *sat, void *elem) {
-  return (steel_avl_node_t *)((uintptr_t)elem + sat->sat_link_offset);
 }
 
 void *steel_avl_search(const steel_avl_tree *sat, const void *key) {
@@ -53,6 +61,8 @@ void steel_avl_node_init(const steel_avl_tree *sat, steel_avl_node_t *san) {
 void *steel_avl_insert(const steel_avl_tree *sat, void *elem) {
   steel_avl_node_t *node;
   steel_avl_node_t *new_node;
+  steel_avl_node_t *unbalanced_node;
+  steel_avl_node_t *unbalanced_node_parent;
   steel_avl_node_t **next_nodep;
   int cmp;
 
@@ -63,6 +73,9 @@ void *steel_avl_insert(const steel_avl_tree *sat, void *elem) {
     sat->sat_root = new_node;
     return elem;
   }
+
+  unbalanced_node = NULL;
+  unbalanced_node_parent = NULL;
 
   node = sat->sat_root;
   next_nodep = NULL;
@@ -78,27 +91,54 @@ void *steel_avl_insert(const steel_avl_tree *sat, void *elem) {
 
     if (*next_nodep == NULL) {
       steel_avl_node_init(new_node);
-      if (cmp < 0) {
-        *next_nodep = new_node;
-      } else {
-        *next_nodep = new_node;
-      }
+      *next_nodep = new_node;
       break;
     }
 
     if (*next_nodep->balance != 0) {
-      unbalanced_node_parent = node;
       unbalanced_node = *next_nodep;
     }
 
     node = *next_nodep;
   }
 
+  // fix-up balance factors
   node = unbalanced_node;
-  // TODO: update balance factor
-  // TODO: handle rotations
+  while (node != NULL && node != new_node) {
+    cmp = sat->sat_comparator(elem, steel_avl_node_to_elem(sat, node));
+    if (cmp < 0) {
+      node->balance -= 1;
+      node = node->san_children[0];
+    } else {
+      node->balance += 1;
+      node = node->san_children[1];
+    }
+  }
 
-  return NULL;
+  // rebalance the tree via rotations
+  node = unbalanced_node;
+  if (node != NULL) {
+    left = no->san_children[0];
+    right = no->san_children[1];
+    if (node->san_balance == -2) {
+      if (node->san_balance == -1) {
+        // left_rotate(P)
+        steel_avl_rotate_left(sat, node);
+      } else if (node->san_balance == 1) {
+        steel_avl_rotate_right(sat, right);
+        steel_avl_rotate_left(sat, node);
+      }
+    } else if (node->san_balance == 2) {
+      if (node->san_balance == 1) {
+        steel_avl_rotate_right(sat, node);
+      } else if (node->san_balance == -1) {
+        steel_avl_rotate_left(sat, left);
+        steel_avl_rotate_right(sat, node);
+      }
+    }
+  }
+
+  return elem;
 }
 
 void steel_avl_fini(steel_avl_tree_t *sat) {
