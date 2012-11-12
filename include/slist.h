5bbccd3c35b06ff6aa729f92d3f165fa97c4951c
Kurtis Nusbaum <klnusbaum@gmail.com>
2012-11-11 16:15:07 -0800

    [slist] finished initial slist
    * Finished the initial implementation of the singly linked list.

--- include/slist.h
+++ include/slist.h
@@ -1,8 +1,6 @@
 #ifndef STEEL_SINGLY_LINKED_LIST_H
 #define STEEL_SINGLY_LINKED_LIST_H
 
-#include "steel.h"
-
 typedef struct steel_slist_node{
   struct steel_slist_node *next;
 };
@@ -17,21 +15,19 @@ typedef struct steel_slist{
 };
 typedef struct steel_slist steel_slist_t;
 
-void steel_slist_init(steel_slist_t *slist, size_t elem_size, size_t link_offset);
+void steel_slist_init(steel_slist_t *ssl, size_t elem_size, size_t link_offset);
 
-void steel_slist_fini(steel_slist_t *slist);
+void steel_slist_fini(steel_slist_t *ssl);
 
-void *steel_slist_insert_head(steel_slist_t *slist, void *elem);
+void *steel_slist_insert_head(steel_slist_t *ssl, void *elem);
 
-void *steel_slist_insert_tail(steel_slist_t *slist, void *elem);
+void *steel_slist_insert_tail(steel_slist_t *ssl, void *elem);
 
-void *steel_slist_insert_after(steel_slist_t *slist, void *after, void *elem);
+void *steel_slist_insert_after(steel_slist_t *ssl, void *after, void *elem);
 
-void *steel_slit_remove_head(steel_slist_t *slist);
+void *steel_slist_remove_head(steel_slist_t *ssl);
 
-void *steel_slist_remove_tail(steel_slist_t *slist);
-
-void *steel_slist_remove(steel_slist_t *slist, void *elem);
+void *steel_slist_remove(steel_slist_t *ssl, void *elem);
 
 
 #endif //STEEL_SINGLY_LINKED_LIST_H
--- src/slist.c
+++ src/slist.c
@@ -2,51 +2,51 @@
 
 #include "assert.h"
 
-void steel_slist_init(steel_slist_t *slist, size_t elem_size, size_t link_offset) {
-  slist->ssl_elem_size = elem_size;
-  slist->ssl_link_offset = link_offset;
+void steel_slist_init(steel_slist_t *ssl, size_t elem_size, size_t link_offset) {
+  ssl->ssl_elem_size = elem_size;
+  ssl->ssl_link_offset = link_offset;
 
-  slist->head = NULL;
-  slist->tail = NULL;
+  ssl->head = NULL;
+  ssl->tail = NULL;
 }
 
-void steel_slist_fini(steel_slist_t *slist) {
-  assert(slist->head == NULL);
-  assert(slist->tail == NULL);
+void steel_slist_fini(steel_slist_t *ssl) {
+  assert(ssl->head == NULL);
+  assert(ssl->tail == NULL);
 }
 
-steel_slist_node_t *steel_slist_elem_to_node(const steel_slist_t *slist, void *elem) {
-  return (steel_slist_node_t *)((uintprt_t)elem + slist->ssl_elem_offset);
+static steel_slist_node_t *steel_slist_elem_to_node(const steel_slist_t *ssl, void *elem) {
+  return (steel_slist_node_t *)((uintprt_t)elem + ssl->ssl_elem_offset);
 }
 
-void *steel_slist_insert_head(steel_slist_t *slist, void *elem) {
+void *steel_slist_insert_head(steel_slist_t *ssl, void *elem) {
   steel_slist_node_t *to_insert;
-  to_insert = steel_slist_elem_to_node(slist, elem);
+  to_insert = steel_slist_elem_to_node(ssl, elem);
 
-  if(slist->head == NULL){
-    slist->head = to_insert;
-    slist->tail = to_insert;
+  if(ssl->head == NULL){
+    ssl->head = to_insert;
+    ssl->tail = to_insert;
     to_insert->next = NULL;
   }
   else{
-    to_insert->next = slist->head;
-    slist->head = to_insert;
+    to_insert->next = ssl->head;
+    ssl->head = to_insert;
   }
 
   return elem;
 }
 
-void *steel_slist_insert_tail(steel_slist_t *slist, void *elem) {
+void *steel_slist_insert_tail(steel_slist_t *ssl, void *elem) {
   steel_slist_node_t *to_insert;
-  to_insert = steel_slist_elem_to_node(slist, elem);
+  to_insert = steel_slist_elem_to_node(ssl, elem);
 
-  if(slist->head == NULL){
-    slist->head = to_insert;
-    slist->tail = to_insert;
+  if(ssl->head == NULL){
+    ssl->head = to_insert;
+    ssl->tail = to_insert;
   }
   else{
-    slist->tail->next = to_insert;
-    slist->tail = to_insert;
+    ssl->tail->next = to_insert;
+    ssl->tail = to_insert;
   }
 
   to_insert->next = NULL;
@@ -54,14 +54,47 @@ void *steel_slist_insert_tail(steel_slist_t *slist, void *elem) {
   return elem;
 }
 
-void *steel_slist_insert_after(steel_slist_t *slist, void *after, void *elem) {
+void *steel_slist_insert_after(steel_slist_t *ssl, void *after, void *elem) {
   steel_slist_node_t *to_insert, *after_node;
-  to_insert = steel_slist_elem_to_node(slist, elem);
-  after_node = steel_slist_elem_to_node(slist, after);
+  to_insert = steel_slist_elem_to_node(ssl, elem);
+  after_node = steel_slist_elem_to_node(ssl, after);
 
   to_insert->next = after_node->next;
   after_node->next = to_insert;
 
   return elem;
+}
+
+static void *steel_slist_node_to_elem(const steel_slist_t *ssl, steel_slist_node_t *elem) {
+    return (void *)((uintptr_t)elem - ssl->ssl_link_offset);
+}
+
+
+void *steel_slist_remove_head(steel_slist_t *ssl) {
+  slist_node_t* old_head;
+  old_head = ssl->head;
+  ssl->head = ssl->head->next;
+  return steel_slist_node_to_elem(ssl, old_head);
+}
+
+void *steel_slist_remove(steel_slist_t *ssl, void *elem) {
+  slist_node_t* to_find;
+  to_find = steel_slist_elem_to_node(ssl, elem);
+
+  /* Special case for head node */
+  if(to_find == ssl->head){
+    ssl->head = ssl->head->next;
+    return elem;
+  }
+
+  slist_node_t* current_node;
+  current_node = ssl->head;
+  while(current_node->next != NULL){
+    if(current->next == to_find){
+      current->next = current->next->next;
+      return elem;
+    }
+  }
+  return NULL;
 }
 
